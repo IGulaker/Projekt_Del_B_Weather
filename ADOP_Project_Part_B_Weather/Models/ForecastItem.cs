ac2a08ea0f1743b37928ed8e458fcf88273db37e
IGulaker <ig.pro@hotmail.com>
2023-04-04 10:21:09 +0200

    Isac Gulaker Projekt Del B (Weather)

---
+++ ADOP_Project_Part_B_Weather/Models/ForecastItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ADOP_Project_Part_B_Weather.Models
+{
+    public class ForecastItem
+    {
+        public DateTime DateTime { get; set; }
+        public double Temperature { get; set; }
+        public double WindSpeed { get; set; }
+        public string Description { get; set; }
+        public string Icon { get; set; }
+        public string Information { get; set; }
+        public override string ToString() => $"Temperatur: {Temperature} degC, Vind: {WindSpeed} m/s";
+    }
+}
---
+++ ADOP_Project_Part_B_Weather/Services/OpenWeatherService.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Json; 
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+using ADOP_Project_Part_B_Weather.Models;
+using System.Collections.Concurrent;
+using static System.Net.WebRequestMethods;
+
+namespace ADOP_Project_Part_B_Weather.Services
+{
+    public class OpenWeatherService
+    {
+        HttpClient httpClient = new HttpClient();
+
+        readonly string apiKey = "ef1d88b7c404add114633c5469439c95";
+
+        public async Task<Forecast> GetForecastAsync(string City)
+        {
+            var language = System.Globalization.CultureInfo.CurrentUICulture.TwoLetterISOLanguageName;
+            var uri = $"https://api.openweathermap.org/data/2.5/forecast?q={City}&units=metric&lang={language}&appid={apiKey}";
+
+            Forecast forecast = await ReadWebApiAsync(uri);
+            return forecast;
+        }
+
+        private async Task<Forecast> ReadWebApiAsync(string uri)
+        {
+            HttpResponseMessage response = await httpClient.GetAsync(uri);
+            response.EnsureSuccessStatusCode();
+            WeatherApiData wd = await response.Content.ReadFromJsonAsync<WeatherApiData>();
+            return WeatherApiDataToForecast(wd);
+        }
+
+        private Forecast WeatherApiDataToForecast(WeatherApiData wData)
+        {
+            var forecast = new Forecast()
+            {
+                City = wData.city.name,
+                Items = wData.list.Select(item => new ForecastItem
+                {
+                    DateTime = UnixTimeStampToDateTime(item.dt),
+                    Temperature = item.main.temp,
+                    WindSpeed = item.wind.speed,
+                    Information = $"Temperatur: {item.main.temp}degC Vind: {item.wind.speed}m/s",
+                    Description = item.weather.FirstOrDefault().description,
+                    Icon = $"http://openweathermap.org/img/w/{item.weather.First().icon}.png"
+                }).ToList()
+            };
+
+            return forecast;
+        }
+
+        private DateTime UnixTimeStampToDateTime(double unixTimeStamp)
+        {
+            DateTime dateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+            dateTime = dateTime.AddSeconds(unixTimeStamp).ToLocalTime();
+            return dateTime;
+        }
+    }
+}
